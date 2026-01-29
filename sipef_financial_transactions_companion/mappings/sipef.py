78f9ef0ba2ca59a3071854be955e4d964c12c3b5
Jin Bok <assistente.ti@grupochavantes.org.br>
2026-01-29 18:30:18 -0300

    feat(sipef_financial_transactions_companion): implement doctipo inference

---
+++ sipef_financial_transactions_companion/mappings/sipef.py
@@ -0,0 +1,137 @@
+# Mapping from Cod. Historico to DocTipo
+COD_HISTORICO_TO_DOCTIPO = {
+    2: 1,  # Cheque
+    5: 1,  # Cheque
+    6: 1,  # Cheque
+    33: 1,  # Cheque
+    53: 1,  # Cheque
+    102: 1,  # Cheque
+    103: 1,  # Cheque
+    106: 1,  # Cheque
+    113: 1,  # Cheque
+    114: 1,  # Cheque
+    153: 1,  # Cheque
+    185: 1,  # Cheque
+    243: 1,  # Cheque
+    274: 1,  # Cheque
+    312: 1,  # Cheque
+    326: 1,  # Cheque
+    408: 1,  # Cheque
+    452: 1,  # Cheque
+    453: 1,  # Cheque
+    456: 1,  # Cheque
+    457: 1,  # Cheque
+    458: 1,  # Cheque
+    598: 1,  # Cheque
+    603: 1,  # Cheque
+    618: 1,  # Cheque
+    653: 1,  # Cheque
+    813: 1,  # Cheque
+    410: 2,  # Depósito
+    411: 2,  # Depósito
+    482: 2,  # Depósito
+    502: 2,  # Depósito
+    503: 2,  # Depósito
+    505: 2,  # Depósito
+    506: 2,  # Depósito
+    510: 2,  # Depósito
+    511: 2,  # Depósito
+    512: 2,  # Depósito
+    513: 2,  # Depósito
+    514: 2,  # Depósito
+    515: 2,  # Depósito
+    516: 2,  # Depósito
+    517: 2,  # Depósito
+    518: 2,  # Depósito
+    519: 2,  # Depósito
+    520: 2,  # Depósito
+    530: 2,  # Depósito
+    605: 2,  # Depósito
+    606: 2,  # Depósito
+    613: 2,  # Depósito
+    623: 2,  # Depósito
+    631: 2,  # Depósito
+    830: 2,  # Depósito
+    910: 2,  # Depósito
+    911: 2,  # Depósito
+    912: 2,  # Depósito
+    913: 2,  # Depósito
+    914: 2,  # Depósito
+    915: 2,  # Depósito
+    916: 2,  # Depósito
+    917: 2,  # Depósito
+    918: 2,  # Depósito
+    919: 2,  # Depósito
+    920: 2,  # Depósito
+    52: 3,  # TED
+    62: 3,  # TED
+    393: 3,  # TED
+    438: 3,  # TED
+    976: 3,  # TED
+    983: 3,  # TED
+    48: 4,  # DOC
+    58: 4,  # DOC
+    63: 4,  # DOC
+    166: 4,  # DOC
+    223: 4,  # DOC
+    397: 4,  # DOC
+    776: 4,  # DOC
+    59: 7,  # Boleto
+    69: 7,  # Boleto
+    109: 7,  # Boleto
+    461: 7,  # Boleto
+    495: 7,  # Boleto
+    9: 13,  # Tarifas Bancárias
+    10: 13,  # Tarifas Bancárias
+    43: 13,  # Tarifas Bancárias
+    44: 13,  # Tarifas Bancárias
+    45: 13,  # Tarifas Bancárias
+    46: 13,  # Tarifas Bancárias
+    47: 13,  # Tarifas Bancárias
+    54: 13,  # Tarifas Bancárias
+    56: 13,  # Tarifas Bancárias
+    127: 13,  # Tarifas Bancárias
+    129: 13,  # Tarifas Bancárias
+    141: 13,  # Tarifas Bancárias
+    142: 13,  # Tarifas Bancárias
+    162: 13,  # Tarifas Bancárias
+    170: 13,  # Tarifas Bancárias
+    181: 13,  # Tarifas Bancárias
+    187: 13,  # Tarifas Bancárias
+    201: 13,  # Tarifas Bancárias
+    205: 13,  # Tarifas Bancárias
+    214: 13,  # Tarifas Bancárias
+    225: 13,  # Tarifas Bancárias
+    231: 13,  # Tarifas Bancárias
+    239: 13,  # Tarifas Bancárias
+    244: 13,  # Tarifas Bancárias
+    256: 13,  # Tarifas Bancárias
+    261: 13,  # Tarifas Bancárias
+    262: 13,  # Tarifas Bancárias
+    263: 13,  # Tarifas Bancárias
+    275: 13,  # Tarifas Bancárias
+    310: 13,  # Tarifas Bancárias
+    311: 13,  # Tarifas Bancárias
+    327: 13,  # Tarifas Bancárias
+    344: 13,  # Tarifas Bancárias
+    353: 13,  # Tarifas Bancárias
+    359: 13,  # Tarifas Bancárias
+    392: 13,  # Tarifas Bancárias
+    405: 13,  # Tarifas Bancárias
+    426: 13,  # Tarifas Bancárias
+    429: 13,  # Tarifas Bancárias
+    431: 13,  # Tarifas Bancárias
+    432: 13,  # Tarifas Bancárias
+    435: 13,  # Tarifas Bancárias
+    442: 13,  # Tarifas Bancárias
+    499: 13,  # Tarifas Bancárias
+    670: 13,  # Tarifas Bancárias
+    286: 15,  # IPVA
+    316: 15,  # IPVA
+    639: 15,  # IPVA
+    833: 15,  # IPVA
+    28: 16,  # Fatura
+    979: 18,  # FGTS
+    445: 19,  # PIX
+    821: 19  # PIX
+}
---
+++ sipef_financial_transactions_companion/sipef_financial_transactions_companion.py
@@ -0,0 +1,66 @@
+#!/user/bin/env python3
+
+"""sipef_financial_transactions_companion
+
+Deterministically infer and fill in the columns
+OperacaoID, Classif.Pagto, Tipo Fornecedor, Class.Fornecedor and DocTipo
+of the SIPEF financial transactions spreadsheet.
+"""
+
+import errno, logging, sys
+from pathlib import Path
+
+import pandas
+
+from mappings.sipef import COD_HISTORICO_TO_DOCTIPO
+
+
+def read_spreadsheet() -> pandas.DataFrame:
+    in_filepath = Path.cwd() / "0200 - Movimentação financeira.xlsx"
+
+    if not in_filepath.exists():
+        raise FileNotFoundError
+
+    return pandas.read_excel(in_filepath, sheet_name=0)
+
+
+def infer_and_fill_columns(data_frame: pandas.DataFrame) -> pandas.DataFrame:
+    data_frame["DocTipo"] = data_frame["Cod. Historico"].apply(
+        lambda x: COD_HISTORICO_TO_DOCTIPO.get(to_int_safe(x), 5)) # Outros
+
+    data_frame = data_frame.drop(columns=["DESPESA CÓDIGO", "Cod. Historico"])
+
+    return data_frame
+
+
+def to_int_safe(value: any) -> int:
+    try:
+        return int(value)
+    except (TypeError, ValueError):
+        return 0
+
+
+def export_spreadsheet(data_frame: pandas.DataFrame) -> None:
+    out_filepath = Path.cwd() / "0200 - Movimentação financeira - SFTC.xlsx"
+    data_frame.to_excel(out_filepath, index=False)
+
+
+if __name__ == "__main__":
+    try:
+        logging.basicConfig(
+            filename=Path.cwd() / "errors.txt",
+            level=logging.ERROR,
+            format="%(asctime)s %(levelname)s: %(message)s",
+        )
+
+        export_spreadsheet(
+            infer_and_fill_columns(
+                read_spreadsheet()
+            )
+        )
+    except FileNotFoundError as e:
+        logging.error("Expected spreadsheet not found.", exc_info=True)
+        sys.exit(errno.ENOENT)
+    except Exception as e:
+        logging.error("Error processing the spreadsheet.", exc_info=True)
+        sys.exit(1)
